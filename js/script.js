3240808176a888166dd4b2a77aa787204f9cf551
Emily Colon <38858811+emilycolon@users.noreply.github.com>
2018-08-21 09:36:34 -0400

    update dragon randomization test 2

--- js/script.js
+++ js/script.js
@@ -124,7 +124,6 @@ function create() {
   dragon.body.gravity.x = game.rnd.integerInRange(-50, 50);
   dragon.body.gravity.y = 20 + Math.random() * 100;
   dragon.body.bounce.setTo(1);
-  dragon.events.onWorldBounds.add(randomizeDragon, this);
 
   // Add a input listener to return to game from being paused
   game.input.onDown.add(unpause, self);
@@ -160,6 +159,8 @@ function createText() {
 
 // This function runs while the game is being played
 function update() {
+  dragon.events.onWorldBounds.add(randomizeDragon, this);
+
   // This listens for overlap on the players and stars and runs collectStar()
   game.physics.arcade.overlap(player, stars, collectStar, null, this);
 
